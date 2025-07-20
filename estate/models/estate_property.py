0a4b986944849205bc7776d0f7979ee44447d585
Krishna Gautam <168166801+Krishna2082g@users.noreply.github.com>
2025-07-20 15:23:59 +0545

    updated code

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 class TestModel(models.Model):
     _name = 'test_model'
     _description = 'Test Model'
@@ -37,11 +37,15 @@ class TestModel(models.Model):
     salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
-
-
-
-
-
+    total_area=fields.Float(compute="_compute_total_area")
+    best_price = fields.Float(string="Best Offer", compute="_compute_best_price")
     
-
-    
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area 
+            
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped("price"), default=0)
